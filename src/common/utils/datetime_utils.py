4335b371cda2cb250abb2a84824e1576539b0272
anhlt <tuananh220695@gmail.com>
2025-08-06 17:16:35 +0700

    chore: save point

--- src/common/utils/datetime_utils.py
+++ src/common/utils/datetime_utils.py
@@ -1,35 +1,13 @@
 from datetime import UTC, datetime, timezone
 
-from uuid_utils import UUID, uuid7
-
 
 def current_utc_timestamp() -> int:
     return round(datetime.now(UTC).timestamp())
 
 
-def timestamp_after_delta(hours: int = 0, minutes: int = 0, seconds: int = 0) -> int:
-    """Return the timestamp after delta time."""
-    return current_utc_timestamp() + hours * 3600 + minutes * 60 + seconds
-
-
-def timestamp_to_hex(timestamp: float | int) -> str:
-    """Convert timestamp (in seconds) to Hexadecimal string"""
-    return uuid7(int(timestamp)).hex[:8]
-
-
-def uuid7_to_timestamp(uuid: str) -> int:
-    """Convert uuid7 string to timestamp"""
-    return UUID(uuid).timestamp
-
-
 def datetime_str_to_timestamp(dt_str: str, dt_format="%Y-%m-%dT%H:%M:%SZ") -> int:
     """Convert ISO 8601 UTC string (e.g., '2023-08-29T03:13:27Z') to Unix timestamp (int)."""
     return int(datetime.strptime(dt_str, dt_format).replace(tzinfo=timezone.utc).timestamp())
-
-
-def format_datetime_str(dt_str: str, dt_format="%Y-%m-%dT%H:%M:%SZ", new_format="%d %B %Y at %I:%M %p") -> str:
-    """Convert a datetime string from one format to another."""
-    return datetime.strptime(dt_str, dt_format).strftime(new_format)
 
 
 def round_n_minutes(dt: datetime, n: int = 5) -> datetime:
--- tests/handlers/master/test_update_agent_deployment.py
+++ tests/handlers/master/test_update_agent_deployment.py
@@ -1,5 +1,7 @@
+import pytest
 from mock import MagicMock
 
+from src.common.exceptions import NotFoundError
 from src.handlers.master.update_agent_deployment.main import handler, notifier
 from tests.conftest import TEST_DIR
 from tests.mock import load_event
@@ -9,8 +11,25 @@ def test_normal_case(agent_repo):
     event = load_event(TEST_DIR / "data" / "cloudformation_event.json")
     notifier.notify = MagicMock(side_effect=lambda *a, **kw: None)
     handler(event, None)
-
     account = agent_repo.get("000000000000")
-    assert account.id == "000000000000"
-    assert account.region == "us-east-1"
     assert account.status == "CREATE_COMPLETE"
+
+    event.detail["status-details"]["status"] = "UPDATE_FAILED"
+    notifier.notify = MagicMock(side_effect=lambda *a, **kw: None)
+    handler(event, None)
+    account = agent_repo.get("000000000000")
+    assert account.status == "UPDATE_FAILED"
+
+    event.detail["status-details"]["status"] = "DELETE_COMPLETE"
+    notifier.notify = MagicMock(side_effect=lambda *a, **kw: None)
+    handler(event, None)
+    account = agent_repo.get("000000000000")
+    assert account.status == "DELETE_COMPLETE"
+
+
+def test_abnormal_case(agent_repo):
+    event = load_event(TEST_DIR / "data" / "health_event.json")
+    handler(event, None)
+
+    with pytest.raises(NotFoundError):
+        agent_repo.get("000000000000")
