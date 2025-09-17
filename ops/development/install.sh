02d93cc0c89a5b0ecba035daba8cd7b4794fcfc4
anhlt <tuananh220695@gmail.com>
2025-09-17 18:15:04 +0700

    refactor: upgrade python version to 3.13

--- ops/development/install.sh
+++ ops/development/install.sh
@@ -14,23 +14,25 @@ source "${SCRIPT_DIR}/base.sh"
 # ==================================================================================
 # INSTALL PYTHON VENV
 # ==================================================================================
-# Ensure Python 3.12 is installed
-if ! command -v python3.12 >/dev/null 2>&1; then
-    echo -e "${RED}Python 3.12 is not installed. Please install it first.${RESET}"
+# Ensure Python 3.13
+PYTHON_BIN=$(command -v python3.13 || true)
+if [[ -z "$PYTHON_BIN" ]]; then
+    echo -e "${RED}Python3.13 is not installed. Please install it first.${RESET}"
     exit 1
 fi
 
 # Install python venv
-if ! python3.12 -m venv --help >/dev/null 2>&1; then
+echo "Using Python interpreter at: $PYTHON_BIN"
+if ! $PYTHON_BIN -m venv --help >/dev/null 2>&1; then
     echo -e "${YELLOW}venv is not installed. Installing venv...${RESET}"
-    python3.12 -m ensurepip --upgrade
-    python3.12 -m pip install --user virtualenv
+    $PYTHON_BIN -m ensurepip --upgrade
+    $PYTHON_BIN -m pip install --user virtualenv
 fi
 
 # Create a virtual environment and activate it
 if [[ ! -d "$PY_VENV" ]]; then
     echo -e "${GREEN}Creating a virtual environment...${RESET}"
-    python3.12 -m venv "$PY_VENV"
+    $PYTHON_BIN -m venv "$PY_VENV"
 fi
 
 if ! source "${PY_VENV}/bin/activate"; then
