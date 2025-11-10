f73097bc538f0d0150b8981af35439ff4f410f84
anhlt <tuananh220695@gmail.com>
2025-11-10 17:22:41 +0700

    chore: save point

--- ops/pyscripts/cli.py
+++ ops/pyscripts/cli.py
@@ -5,18 +5,35 @@ Entrypoint for the CLI tool.
 import sys
 from pathlib import Path
 
+import click
 from rich import pretty, traceback
 
 BASE_DIR = Path(__file__).parent
 sys.path.append(str(BASE_DIR))
 
 
-def cli():
-    traceback.install()
+def install_context(ctx: click.Context):
+    ctx.ensure_object(dict)
+    ctx.obj["session"] = "session"
+    return ctx
+
+
+@click.group()
+@click.option("--debug/--no-debug", default=False, required=False, help="Enable debug mode.")
+@click.pass_context
+def cli(ctx, debug):
+    if debug:
+        traceback.install()
     pretty.install()
+    install_context(ctx)
+
+    @ctx.call_on_close
+    def close():
+        print("Exiting CLI tool.")
 
 
 def main():
+    # cli.add_command(command)
     cli()
 
 
